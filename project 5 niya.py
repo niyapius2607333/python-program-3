f9b38b892a4d2d2c7f5c58e342ff7d4d2872aa68
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-08 14:45:20 +0530

    Add files via upload

---
+++ project 5 niya.py
@@ -0,0 +1,8 @@
+'''
+Author : Niya Pius
+Date : 05-10-2024
+Python program to print string in a specific pattern
+Version 1.0
+'''
+str="Twinkle, twinkle,little star,\n\t How I wonder what you are! \n\t\t Up above the world so high,\n\t\t Like a diamond in the sky.\nTwinkle,twinkle ,little star,\n\tHow I wonder what you are"
+print(str)
