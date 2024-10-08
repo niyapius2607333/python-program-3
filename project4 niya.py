5dd56c306895cc177a1599f2ef0832a54096a3b7
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-08 14:44:50 +0530

    Add files via upload

---
+++ project4 niya.py
@@ -0,0 +1,11 @@
+'''
+Author : Niya Pius
+Date : 05-10-2024
+Python program to do calculations using math module
+Version 1.0
+'''
+import math
+number=int(input("Enter the number"))
+print("square root of the number",':',math.sqrt(number))
+print("factorial of the number",':',math.factorial(number))
+print("number raised to power 2:",math.pow(number,2))
