efa30fc671a0b3215b94b2a7ff92abe5e326c790
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-08 14:43:40 +0530

    Add files via upload

---
+++ project 3 niya.py
@@ -0,0 +1,21 @@
+'''
+Author : Niya Pius
+Date : 05-10-2024
+Python program to do arithmetic operations and compare two numbers
+Version 1.0
+'''
+num1=10
+num2=5
+sum=num1+num2
+div=num1/num2
+print("sum=",sum,"division",div)
+com1=num1>num2
+com2=num1==num2
+log1=(num1>num2)and(num2<num1)
+log2=(num1>num2)or(num2<num1)
+print("Is num1 greater than num2?:",com1)
+print("are num1 and num2 are equal:",com2)
+print("Logical AND:",log1)
+print("Logical OR:",log2)
+
+
